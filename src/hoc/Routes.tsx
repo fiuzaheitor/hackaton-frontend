04a08b41bcfb105398df842c3799e909f49c468f
Heitor <163003601+fiuzaheitor@users.noreply.github.com>
2024-10-16 13:39:37 -0300

    Rotas protegidas - 13:39 - 16/10

--- src/hoc/Routes.tsx
+++ src/hoc/Routes.tsx
@@ -1,21 +1,83 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes, Navigate, useNavigate, useLocation } from 'react-router-dom'
 import { Login } from '../pages/auth/Login'
 import { Signup } from '../pages/auth/Signup'
 import { Home } from '../pages/dashboard/Home/'
+import { getCookie } from '../utils/cookies'
 
+const routes = [
+    {
+        "path": "/login",
+        "component": Login,
+        "protect": false
+    },
+    {
+        "path": "/signup",
+        "component": Signup,
+        "protect": false
+    },
+    {
+        "path": "/home",
+        "component": Home,
+        "protect": true
+    }
+]
 
-const AppRoutes: React.FC = () => {
+function ProtectedRoute() {
+    const auth = JSON.parse(getCookie('_bu_l') as string)
+    const location = useLocation()
+    const navigate = useNavigate()
+  
+    const [previousPath, setPreviousPath] = useState<string | null>(sessionStorage.getItem('previousPath') || null)
+
+    useEffect(() => {
+        const currentPath = location.pathname
+        if (previousPath !== currentPath) {
+          sessionStorage.setItem('previousPath', currentPath) // Armazena o caminho anterior
+          setPreviousPath(currentPath)
+        }
+      }, [location, previousPath])
+
+    useEffect(() => {
+    const isAuthenticated = auth?.ui
+
+    const routeCurrent = routes.find((route: any) => {
+        return route.path.split('/')[1] === location.pathname.split('/')[1]
+    })
+
+    if (isAuthenticated) {
+        if (!routeCurrent?.protect) {
+        previousPath ? navigate(previousPath) : navigate('/home')
+        }
+    } else {
+        if (routeCurrent?.protect) {
+        navigate('/login')
+        }
+    }
+    }, [auth, location, navigate])
+
+    const isAuthenticated = auth?.ui
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" Component={Login}/>
-                <Route path="/login" Component={Login} />
-                <Route path="/signup" Component={Signup} />
-                <Route path="/home" Component={Home} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            {
+                isAuthenticated ?
+                routes.filter((route: any) => route.protect).map((route: any, index: any) => {
+                    return <Route key={index} path={route.path} element={React.createElement(route.component)} />
+                })
+                :
+                routes.filter((route: any) => !route.protect).map((route: any, index: any) => {
+                    return <Route key={index} path={route.path} element={React.createElement(route.component)} />
+                })
+            }
+        </Routes>
     )
 }
 
-export default AppRoutes
+export default function AppRoute() {
+    return (
+        <BrowserRouter>
+            <ProtectedRoute />
+        </BrowserRouter>
+    )
+}
--- src/pages/dashboard/Home/index.tsx
+++ src/pages/dashboard/Home/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import './styles.scss'
 import { Header } from "../../../components/Header";
 import { Calendar } from "../../../components/Calendar";
+import { getCookie } from "../../../utils/cookies";
 
 export const Home: React.FC = () => {
+    const auth = JSON.parse(getCookie('_bu_l') as string)
+
     return (
         <section className="container__home">
             <Header />
